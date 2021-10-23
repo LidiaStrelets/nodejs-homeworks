0bd032eff1ce037bc182312f4b21f89ba2b0460b
LidiaStrelets <0630610171li@gmail.com>
2021-10-23 15:15:38 +0300

    feat: pagination

--- controllers/contacts/getContacts.js
+++ controllers/contacts/getContacts.js
@@ -1,6 +1,8 @@
 const { Contact } = require("./../../models/contacts");
 
 const getContacts = async (req, res, next) => {
+  const { page = 1, limit = 10 } = req.query;
+
   if (req.query.favorite) {
     const favoriteContacts = await Contact.find({
       owner: req.user,
@@ -13,7 +15,10 @@ const getContacts = async (req, res, next) => {
     });
   }
 
-  const allContacts = await Contact.find({ owner: req.user });
+  const allContacts = await Contact.find({ owner: req.user }, "", {
+    skip: (page - 1) * limit,
+    limit,
+  });
   res.json({
     status: "Succeed",
     code: 200,
